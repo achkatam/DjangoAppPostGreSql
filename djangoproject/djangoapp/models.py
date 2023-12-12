cfac78873d75449bbe988bc42335a87164e96eab
achkatam <agmatevv@gmail.com>
2023-12-11 20:21:16 -0500

    added exception handling, pgadmin service so I can use the 5000port as POSTGRESQL client, create superuser

--- djangoproject/djangoapp/models.py
+++ djangoproject/djangoapp/models.py
@@ -7,3 +7,7 @@ class User(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=100)  # Don't need email validations, that is why is charfield
     age = models.IntegerField()
+
+    def __str__(self):
+        self.full_name = self.first_name + ' ' + self.last_name
+        return f'{self.full_name} - {self.email} - {self.age}'
